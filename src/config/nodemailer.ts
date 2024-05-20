109002485f49c04470386eac16b9f1c90ea5474a
Edwin Hernandez Espino <he377582@uaeh.edu.mx>
2024-05-20 16:12:35 -0600

    correo

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -7,7 +7,6 @@ const config = () => {
     return {
         host: process.env.SMTP_HOST,
         port: +process.env.SMTP_PORT,
-        secure: false,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
