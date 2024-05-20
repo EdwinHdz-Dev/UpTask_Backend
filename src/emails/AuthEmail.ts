b4b1e685f21e55471d1ede6b12c1288f3e5b2fa8
Edwin Hernandez Espino <he377582@uaeh.edu.mx>
2024-05-20 12:58:14 -0600

    correo

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -8,35 +8,43 @@ interface IEmail {
 
 export class AuthEmail {
     static sendConfirmationEmail = async (user: IEmail) => {
-        const info = await resend.emails.send({
-            from: 'UpTask <admin@task.com>',
-            to: user.email,
-            subject: 'UpTask - Confirma tu cuenta',
-            html: `<p>Hola: ${user.name}, has creado tu cuenta en UpTask,
-            ya casi está todo listo, solo debes confirmar tu cuenta</p>
-                <p>Visita el siguiente enlace:</p>
-                <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
-                <p>E ingresa el código: <b>${user.token}</b></p>
-                <p>Este token expira en 10 minutos</p>
-            `
-        });
+        try {
+            const info = await resend.emails.send({
+                from: 'UpTask <admin@task.com>',
+                to: user.email,
+                subject: 'UpTask - Confirma tu cuenta',
+                html: `<p>Hola: ${user.name}, has creado tu cuenta en UpTask,
+                ya casi está todo listo, solo debes confirmar tu cuenta</p>
+                    <p>Visita el siguiente enlace:</p>
+                    <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
+                    <p>E ingresa el código: <b>${user.token}</b></p>
+                    <p>Este token expira en 10 minutos</p>
+                `
+            });
 
-        console.log('Mensaje enviado');
+            console.log('Mensaje enviado', info);
+        } catch (error) {
+            console.error('Error enviando el correo:', error);
+        }
     };
 
     static sendPasswordResetToken = async (user: IEmail) => {
-        const info = await resend.emails.send({
-            from: 'UpTask <admin@task.com>',
-            to: user.email,
-            subject: 'UpTask - Restablece tu contraseña',
-            html: `<p>Hola: ${user.name}, has solicitado restablecer tu contraseña.</p>
-                <p>Visita el siguiente enlace:</p>
-                <a href="${process.env.FRONTEND_URL}/auth/new-password">Restablecer Contraseña</a>
-                <p>E ingresa el código: <b>${user.token}</b></p>
-                <p>Este token expira en 10 minutos</p>
-            `
-        });
+        try {
+            const info = await resend.emails.send({
+                from: 'UpTask <admin@task.com>',
+                to: user.email,
+                subject: 'UpTask - Restablece tu contraseña',
+                html: `<p>Hola: ${user.name}, has solicitado restablecer tu contraseña.</p>
+                    <p>Visita el siguiente enlace:</p>
+                    <a href="${process.env.FRONTEND_URL}/auth/new-password">Restablecer Contraseña</a>
+                    <p>E ingresa el código: <b>${user.token}</b></p>
+                    <p>Este token expira en 10 minutos</p>
+                `
+            });
 
-        console.log('Mensaje enviado');
+            console.log('Mensaje enviado', info);
+        } catch (error) {
+            console.error('Error enviando el correo:', error);
+        }
     };
 }
